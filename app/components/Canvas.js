e0f1767f6ae91f4f6ad8626283d39caf09ff0173
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-12-11 02:32:12 +0530

    added pen width/stroke

--- app/components/Canvas.js
+++ app/components/Canvas.js
@@ -1,10 +1,13 @@
 "use client"
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { fabric } from 'fabric';
 
 const Canvas = () => {
   const canvasRef = useRef(null);
   const canvas = useRef(null);
+
+  const [penColor, setPenColor] = useState('black');
+  const [penWidth, setPenWidth] = useState(5);
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
@@ -22,7 +25,7 @@ const Canvas = () => {
         width: 50,
         height: 50,
         fill: 'blue',
-         selectable: true,
+        selectable: true,
       });
       canvas.current.add(square);
     }
@@ -42,8 +45,8 @@ const Canvas = () => {
   const handlePenDraw = () => {
     if (canvas.current) {
       canvas.current.isDrawingMode = !canvas.current.isDrawingMode;
-      canvas.current.freeDrawingBrush.color = 'black';
-      canvas.current.freeDrawingBrush.width = 5;
+      canvas.current.freeDrawingBrush.color = penColor;
+      canvas.current.freeDrawingBrush.width = penWidth;
     }
   };
 
@@ -75,13 +78,33 @@ const Canvas = () => {
   }, []);
 
   return (
-    <div className='m-5 p-5 flex flex-col items-center'>
-    <div>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
-      <button className='p-2 m-2 border border-slate-400 rounded' onClick={handleSquareDraw}>Draw Square</button>
-      <button className='p-2 m-2 border border-slate-400 rounded' onClick={handleCircleDraw}>Draw Circle</button>
-      <button className='p-2 m-2 border border-slate-400 rounded' onClick={handlePenDraw}>Pen</button>
-      <button className='p-2 m-2 border border-slate-400 rounded' onClick={handleEraser}>Eraser</button>
+    <div className='m-2 p-5 flex flex-col items-center'>
+    <div className='flex items-center'>
+      <input className='w-56 rounded bg-slate-400' type="file" accept="image/*" onChange={handleImageUpload} />
+      <button className='p-2 m-2 border border-slate-400 rounded bg-sky-600 text-white' onClick={handleSquareDraw}>Draw Square</button>
+      <button className='p-2 m-2 border border-slate-400 rounded bg-sky-600 text-white' onClick={handleCircleDraw}>Draw Circle</button>
+      <button className='p-2 m-2 border border-slate-400 rounded bg-sky-600 text-white' onClick={handlePenDraw}>Pen</button>
+      <button className='p-2 m-2 border border-slate-400 rounded bg-sky-600 text-white' onClick={handleEraser}>Eraser</button>
+
+        <label className='mr-2'>
+         Pen Color:
+        <input
+          className=''
+          type="color"
+          value={penColor}
+          onChange={(e) => setPenColor(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Pen Width: 
+        <input
+          className='border border-slate-400 w-12 rounded'
+          type="number"
+          value={penWidth}
+          onChange={(e) => setPenWidth(Number(e.target.value))}
+        />
+      </label>
     </div>
       
       <canvas className='border border-slate-500' ref={canvasRef} />
